5b860b2a8cf9c0cae324feb4e1e1e799c1c8ac35
nileshkumar <nileshkumar1>
2022-12-01 23:46:21 +0530

    Add Some Thing

--- emp/admin.py
+++ emp/admin.py
@@ -4,7 +4,21 @@ from .models import *
 class Emp_Admin(admin.ModelAdmin):
     list_display = ('emp_id', 'emp_name', 'mobile', 'email','position','experience')
     
-class Attendance(admin.ModelAdmin):
+class Attendance_Admin(admin.ModelAdmin):
     list_display= ('In_time', 'Out_time')
+    
+class leave_Admin(admin.ModelAdmin):
+    list_display = ('Leave_Date', 'Resion', 'Status')
 
+class Department_Admin(admin.ModelAdmin):
+    list_display = ('Name', 'Type', 'Total_emp')
+    
+class Emergency_contact_Admin(admin.ModelAdmin):
+    list_display = ('Name', 'Address', 'contact')
+    
+    
 admin.site.register(Emp,Emp_Admin)
+admin.site.register(Attendance,Attendance_Admin)
+admin.site.register(Leave,leave_Admin)
+admin.site.register(Department,Department_Admin)
+admin.site.register(Emergency_contact,Emergency_contact_Admin)
